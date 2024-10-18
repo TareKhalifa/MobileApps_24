a13b0a4212b754702d41315776eb120e947ba9e0
Tarek Khalifa <tarekelsayedkhalifa@gmail.com>
2024-10-18 18:48:28 +0300

    Week3

--- lib/main.dart
+++ lib/main.dart
@@ -55,71 +55,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  final _formKey = GlobalKey<FormState>();
+  bool valid = false;
+  String pic = "";
+  Map<String, String> mp = {
+    'cat': 'cat.jpg',
+    'dog': 'dog.jpg',
+  };
+  // Create a controller to retrieve the text input from the TextFormField
+  final TextEditingController _nameController = TextEditingController();
+  void _showImage(String text) {
+    text = text.toLowerCase();
+    if (mp.keys.any((key) => text.contains(key))) {
+      pic = mp[mp.keys.firstWhere((key) => text.contains(key))]!;
+      valid = true;
+    } else {
+      valid = false;
+    }
+    setState(() {});
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _nameController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Form Example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey, // Associate the form key
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Enter text here',
+                ),
+                // onChanged: (value)
+                //   setState(() {
+                //     // Update the value as the user types
+                //   });
+                // },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter text here';
+                  }
+                  return null;
+                },
+              ),
+/*
+              Text(_nameController.text.toUpperCase()),
+*/
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState?.validate() == true) {
+                    // Process data if the form is valid
+                    _showImage(_nameController.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(_nameController.text)),
+                    );
+                  }
+                },
+                child: Text('Submit'),
+              ),
+              if (valid)
+                Image.asset(
+                  "assets/images/" + pic,
+                  width: 100,
+                  height: 100,
+                )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
