c879abf01c85f9dd40157cfbe6ea2e08e970e3e4
Tarek Khalifa <tarekelsayedkhalifa@gmail.com>
2024-10-19 11:33:45 +0300

    Week4 without returning data with pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,128 @@
 import 'package:flutter/material.dart';
+class CustomCard extends StatelessWidget {
+  final String name;
+  final String picturePath;
+  CustomCard({required this.name, required this.picturePath});
 
-void main() {
-  runApp(const MyApp());
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(name),
+          Image.asset(picturePath, width: 400, height: 400,),
+          ElevatedButton(
+              onPressed: ()=>Navigator.pop(context),
+              child: Text("Return to first screen"))
+        ],
+      ) ,
+    );
+  }
 }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FirstScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class FirstScreen extends StatelessWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
   final _formKey = GlobalKey<FormState>();
-  bool valid = false;
-  String pic = "";
-  Map<String, String> mp = {
-    'cat': 'cat.jpg',
-    'dog': 'dog.jpg',
-  };
-  // Create a controller to retrieve the text input from the TextFormField
   final TextEditingController _nameController = TextEditingController();
-  void _showImage(String text) {
-    text = text.toLowerCase();
-    if (mp.keys.any((key) => text.contains(key))) {
-      pic = mp[mp.keys.firstWhere((key) => text.contains(key))]!;
-      valid = true;
-    } else {
-      valid = false;
-    }
-    setState(() {});
-  }
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    _nameController.dispose();
-    super.dispose();
-  }
+  final TextEditingController _typeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Form Example'),
+        title: const Text('First Screen'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Center(
         child: Form(
-          key: _formKey, // Associate the form key
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'Enter text here',
-                ),
-                // onChanged: (value)
-                //   setState(() {
-                //     // Update the value as the user types
-                //   });
-                // },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter text here';
-                  }
-                  return null;
-                },
-              ),
-/*
-              Text(_nameController.text.toUpperCase()),
-*/
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState?.validate() == true) {
-                    // Process data if the form is valid
-                    _showImage(_nameController.text);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text(_nameController.text)),
-                    );
-                  }
-                },
-                child: Text('Submit'),
-              ),
-              if (valid)
-                Image.asset(
-                  "assets/images/" + pic,
-                  width: 100,
-                  height: 100,
-                )
-            ],
+        key: _formKey, // Associate the form key
+        child: Column(
+          children: <Widget>[
+          TextFormField(
+          controller: _typeController,
+          decoration: InputDecoration(
+            labelText: 'Enter type here',
           ),
+          validator: (value) {
+            if (value=="cat" || value=='dog') {
+              return null;
+            }
+            return "Please enter cat or dog";
+          },
         ),
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Enter the name here',
+              ),
+              validator: (value) {
+                if (value==null || value.isEmpty) {
+                  return "Please enter a name";
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState?.validate() == true) {
+                    Navigator.push(context,MaterialPageRoute(
+                      builder: (context) =>
+                          SecondScreen(name: _nameController.text,
+                            imagePath:"assets/images/${_typeController.text}.jpg"),
+                    ));
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ]
       ),
-    );
+    )));
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  final String name;
+  final String imagePath;
+
+  const SecondScreen({required this.name, required this.imagePath});
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  late String name;
+  late String imagePath;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize state with the values passed from the first screen
+    name = widget.name;
+    imagePath = widget.imagePath;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+          child: CustomCard(name: name,
+              picturePath: imagePath),
+    ));
   }
 }
