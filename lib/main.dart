ac23f1a32bc72b40b8b44e88c7a54c2f526ca6b4
Tarek Khalifa <tarekelsayedkhalifa@gmail.com>
2024-10-19 11:46:47 +0300

    Week4 - after returning data from second screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+
 class CustomCard extends StatelessWidget {
   final String name;
   final String picturePath;
+  final TextEditingController _animalScore = TextEditingController();
+
   CustomCard({required this.name, required this.picturePath});
 
   @override
@@ -12,30 +15,61 @@ class CustomCard extends StatelessWidget {
         children: [
           Text(name),
           Image.asset(picturePath, width: 400, height: 400,),
+          TextFormField(
+            controller: _animalScore,
+            decoration: InputDecoration(
+              labelText: 'Enter the rating here',
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Please enter a rating";
+              }
+              return null;
+            },
+          ),
           ElevatedButton(
-              onPressed: ()=>Navigator.pop(context),
-              child: Text("Return to first screen"))
+            onPressed: () {
+              if (_animalScore.text.isNotEmpty) {
+                Navigator.pop(context, {"currentRating": _animalScore.text});
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("Please enter a rating")),
+                );
+              }
+            },
+            child: Text("Return to first screen"),
+          )
         ],
-      ) ,
+      ),
     );
   }
 }
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirstScreen(),
+      home: FirstScreen("None"),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  final String currentRating;
 
+  FirstScreen(this.currentRating);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _typeController = TextEditingController();
+  String currentRating = "None";
 
   @override
   Widget build(BuildContext context) {
@@ -45,49 +79,62 @@ class FirstScreen extends StatelessWidget {
       ),
       body: Center(
         child: Form(
-        key: _formKey, // Associate the form key
-        child: Column(
-          children: <Widget>[
-          TextFormField(
-          controller: _typeController,
-          decoration: InputDecoration(
-            labelText: 'Enter type here',
-          ),
-          validator: (value) {
-            if (value=="cat" || value=='dog') {
-              return null;
-            }
-            return "Please enter cat or dog";
-          },
-        ),
-            TextFormField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Enter the name here',
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _typeController,
+                decoration: InputDecoration(
+                  labelText: 'Enter type here',
+                ),
+                validator: (value) {
+                  if (value == "cat" || value == 'dog') {
+                    return null;
+                  }
+                  return "Please enter cat or dog";
+                },
               ),
-              validator: (value) {
-                if (value==null || value.isEmpty) {
-                  return "Please enter a name";
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState?.validate() == true) {
-                    Navigator.push(context,MaterialPageRoute(
-                      builder: (context) =>
-                          SecondScreen(name: _nameController.text,
-                            imagePath:"assets/images/${_typeController.text}.jpg"),
-                    ));
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ]
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Enter the name here',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a name";
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState?.validate() == true) {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondScreen(
+                          name: _nameController.text,
+                          imagePath: "assets/images/${_typeController.text}.jpg",
+                        ),
+                      ),
+                    );
+
+                    if (result != null && result.containsKey('currentRating')) {
+                      setState(() {
+                        currentRating = result['currentRating'];
+                      });
+                    }
+                  }
+                },
+                child: Text('Submit'),
+              ),
+              Text('Current Rating: $currentRating'),
+            ],
+          ),
+        ),
       ),
-    )));
+    );
   }
 }
 
@@ -113,7 +160,6 @@ class _SecondScreenState extends State<SecondScreen> {
     imagePath = widget.imagePath;
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -121,8 +167,11 @@ class _SecondScreenState extends State<SecondScreen> {
         title: const Text('Second Screen'),
       ),
       body: Center(
-          child: CustomCard(name: name,
-              picturePath: imagePath),
-    ));
+        child: CustomCard(
+          name: name,
+          picturePath: imagePath,
+        ),
+      ),
+    );
   }
 }
